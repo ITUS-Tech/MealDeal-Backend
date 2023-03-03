f5f26b1c695e01b0627d962b9a6b2a7ca4081eb3
Samee <abdussamee602@gmail.com>
2023-03-03 01:03:36 -0500

    .

---
+++ src/main/java/com/itus/mealdeal/MealDealApplication.java
@@ -0,0 +1,13 @@
+package com.itus.mealdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealDealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itus/mealdeal/MealDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itus.mealdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
