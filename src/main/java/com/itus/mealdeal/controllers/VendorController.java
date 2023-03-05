f5be581aa06d20100f1335d7812e2d931f9b052b
Samee <abdussamee602@gmail.com>
2023-03-04 21:13:55 -0500

    .

--- src/main/java/com/itus/mealdeal/controllers/VendorController.java
+++ src/main/java/com/itus/mealdeal/controllers/VendorController.java
@@ -25,12 +25,12 @@ public class VendorController {
 	@Autowired
 	VendorRepository vendorRepo;
 	
-	@PostMapping("/")
-	public void addVendor(@RequestBody Vendor vendor) {
-		vendorRepo.save(vendor);
-	}
-	
-	@GetMapping("/")
+//	@PostMapping("")
+//	public void addVendor(@RequestBody Vendor vendor) {
+//		vendorRepo.save(vendor);
+//	}
+//	
+	@GetMapping("")
 	public List<Vendor> getAllVendor() {
 		List<Vendor> list= new ArrayList<>();
 		vendorRepo.findAll().stream().forEach(list::add);
