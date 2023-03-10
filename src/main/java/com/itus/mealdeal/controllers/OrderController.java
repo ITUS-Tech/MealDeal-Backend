ef43c9c17207ecf655ff2b5da639a0140687e1a7
Samee <abdussamee602@gmail.com>
2023-03-10 06:13:27 -0500

    modified order controller

--- src/main/java/com/itus/mealdeal/controllers/OrderController.java
+++ src/main/java/com/itus/mealdeal/controllers/OrderController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itus.mealdeal.models.Order;
+import com.itus.mealdeal.models.User;
 import com.itus.mealdeal.repos.OrderRepository;
+import com.itus.mealdeal.repos.UserRepository;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -20,8 +24,21 @@ public class OrderController {
 	@Autowired
 	OrderRepository orderRepo;
 	
+	@Autowired
+	UserRepository userRepo;
+	
 	@GetMapping("/{userId}")
 	public List<Order> getAllVendorOrders(@PathVariable("userId") int id){
 		return orderRepo.findAllByVendorId(id);
+	}
+	
+	@PostMapping("")
+	public void createOrder(@RequestBody Order order) {
+		User user= userRepo.findById(order.getUserId()).get();
+		order.setCustomerName(user.getFname()+" "+user.getLname());
+		order.setAddress(user.getAddress());
+		order.setPhone(user.getPhno());
+		order.setEmail(user.getEmail());
+		orderRepo.save(order);
 	}
 }
